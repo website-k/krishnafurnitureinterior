5c79d84de6668f0bf781535d4b168d4385ea2d1b
kajal7416 <kajal9054yadav@gmail.com>
2025-10-29 09:25:50 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './style.css';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/krishnafurnitureinterior/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
